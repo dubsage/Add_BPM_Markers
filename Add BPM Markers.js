2d503adbffa058087092f20e1995b4fc7dde76f4
Dubby Sage <dubsage@hotmail.com>
2023-11-05 03:06:23 +0500

    Update Add BPM Markers.js

--- Add BPM Markers.js
+++ Add BPM Markers.js
@@ -6,7 +6,7 @@ function createUI() {
     var checkboxFR = intervalGroup.add("checkbox", undefined, "Stick to framerate?");
     checkboxFR.value = true;
 	
-	var checkboxUOB = intervalGroup.add("checkbox", undefined, "Use only bars?");
+    var checkboxUOB = intervalGroup.add("checkbox", undefined, "Use only bars?");
     checkboxUOB.value = false;
 	
     intervalGroup.add("statictext", undefined, "BPM:");
@@ -20,28 +20,28 @@ function createUI() {
         var bpm = parseFloat(intervalInput.text);
         var comp = app.project.activeItem;
 	    
-	    var frameRate = comp.frameRate;
-	    var duration = comp.duration;
+	var frameRate = comp.frameRate;
+	var duration = comp.duration;
 	    
         var layer = comp.layers.addSolid([0,0,0], bpm + " BPM Markers", comp.width, comp.height, comp.pixelAspect, comp.duration);
         var markerCount = Math.floor(duration / bpm);
 	    
-	    var beatsPerSecond = 60 / bpm;
-	    var count = duration * bpm / 60;
+	var beatsPerSecond = 60 / bpm;
+	var count = duration * bpm / 60;
 		
-	    if (checkboxUOB.value) {
-			beatsPerSecond = 60 / (bpm / 4);
-			count = duration * (bpm / 4) / 60;
-		}
+	if (checkboxUOB.value) {
+	    beatsPerSecond = 60 / (bpm / 4);
+	    count = duration * (bpm / 4) / 60;
+	}
 		
         for (i = 0; i < count; i++) {
-	        var markerTime = i * beatsPerSecond;
-	        if (checkboxFR.value) {
-	            markerTime = (Math.round(markerTime * frameRate))/frameRate;
-	        }
-			layer.property("ADBE Marker").setValueAtTime(markerTime, new MarkerValue((i % 4) + 1));
+	    var markerTime = i * beatsPerSecond;
+	    if (checkboxFR.value) {
+	        markerTime = (Math.round(markerTime * frameRate))/frameRate;
+	    }
+	    layer.property("ADBE Marker").setValueAtTime(markerTime, new MarkerValue((i % 4) + 1));
         }
-		window.close();
+	window.close();
     };	
     window.show();
 }
