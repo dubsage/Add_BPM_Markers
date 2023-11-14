1a1d7f267b7a25ba8f965eb27b8652bbec00be82
Dubby Sage <dubsage@hotmail.com>
2023-11-14 15:07:41 +0500

    Update Add BPM Markers.js

--- Add BPM Markers.js
+++ Add BPM Markers.js
@@ -1,50 +1,73 @@
 function createUI() {
-    var window = new Window("dialog", "Add Markers");
-    window.alignChildren = "left";
-	
-    var intervalGroup = window.add("group");
-    var checkboxFR = intervalGroup.add("checkbox", undefined, "Stick to framerate?");
-    checkboxFR.value = true;
-	
-	var checkboxUOB = intervalGroup.add("checkbox", undefined, "Use only bars?");
-    checkboxUOB.value = false;
-	
-    intervalGroup.add("statictext", undefined, "BPM:");
-    var intervalInput = intervalGroup.add("edittext", undefined, "140");
-    intervalInput.characters = 4;
-	
-    var buttonGroup = window.add("group");
-    var addButton = buttonGroup.add("button", undefined, "Add Markers");
-	
-    addButton.onClick = function() {    
-        var bpm = parseFloat(intervalInput.text);
-        var comp = app.project.activeItem;
-	    
-	    var frameRate = comp.frameRate;
-	    var duration = comp.duration;
-	    
-        var layer = comp.layers.addSolid([0,0,0], bpm + " BPM Markers", comp.width, comp.height, comp.pixelAspect, comp.duration);
-        var markerCount = Math.floor(duration / bpm);
-	    
-	    var beatsPerSecond = 60 / bpm;
-	    var count = duration * bpm / 60;
-		
-	    if (checkboxUOB.value) {
-			beatsPerSecond = 60 / (bpm / 4);
-			count = duration * (bpm / 4) / 60;
-		}
-		
-        for (i = 0; i < count; i++) {
-	        var markerTime = i * beatsPerSecond;
-	        if (checkboxFR.value) {
-	            markerTime = (Math.round(markerTime * frameRate))/frameRate;
-	        }
-			layer.property("ADBE Marker").setValueAtTime(markerTime, new MarkerValue((i % 4) + 1));
-        }
-		layer.enabled = false; 
-		layer.locked = true; 
-		window.close();
-    };	
-    window.show();
+  var window = new Window("dialog", "Add Markers");
+  window.alignChildren = "left";
+
+  var beatDelayGroup = window.add("group");
+  beatDelayGroup.add(
+    "statictext",
+    undefined,
+    "Start numbering from this beat: "
+  );
+  var beatDelayInput = beatDelayGroup.add("edittext", undefined, "1");
+  beatDelayInput.characters = 1;
+
+  var intervalGroup = window.add("group");
+  var checkboxFR = intervalGroup.add(
+    "checkbox",
+    undefined,
+    "Stick to framerate?"
+  );
+  checkboxFR.value = true;
+
+  var checkboxUOB = intervalGroup.add("checkbox", undefined, "Use only bars?");
+  checkboxUOB.value = false;
+
+  intervalGroup.add("statictext", undefined, "BPM:");
+  var intervalInput = intervalGroup.add("edittext", undefined, "140");
+  intervalInput.characters = 4;
+
+  var buttonGroup = window.add("group");
+  var addButton = buttonGroup.add("button", undefined, "Add Markers");
+
+  addButton.onClick = function () {
+    var bpm = parseFloat(intervalInput.text);
+    var comp = app.project.activeItem;
+    var beatDelay = parseFloat(beatDelayInput.text) - 1;
+
+    var frameRate = comp.frameRate;
+    var duration = comp.duration;
+
+    var layer = comp.layers.addSolid(
+      [0, 0, 0],
+      bpm + " BPM Markers",
+      comp.width,
+      comp.height,
+      comp.pixelAspect,
+      comp.duration
+    );
+    var markerCount = Math.floor(duration / bpm);
+
+    var beatsPerSecond = 60 / bpm;
+    var count = (duration * bpm) / 60;
+
+    if (checkboxUOB.value) {
+      beatsPerSecond = 60 / (bpm / 4);
+      count = (duration * (bpm / 4)) / 60;
+    }
+
+    for (i = 0; i < count; i++) {
+      var markerTime = i * beatsPerSecond;
+      if (checkboxFR.value) {
+        markerTime = Math.round(markerTime * frameRate) / frameRate;
+      }
+      layer
+        .property("ADBE Marker")
+        .setValueAtTime(markerTime, new MarkerValue(((i + beatDelay) % 4) + 1));
+    }
+    layer.enabled = false;
+    layer.locked = true;
+    window.close();
+  };
+  window.show();
 }
 createUI();
